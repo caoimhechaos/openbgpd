d33e673b290637e9127a8c4ffef6cb57c7b6a525
deraadt <deraadt>
2003-12-22 06:42:19 +0000

    spelling

--- rde_rib.c
+++ rde_rib.c
@@ -30,9 +30,9 @@
 /*
  * BGP RIB -- Routing Information Base
  *
- * The RIB is build with one aspect in mind. Speed -- acctually update speed.
- * Therefor one thing needs to be absolutly avoided, long table walks.
- * This is achieved by heavly linking the different parts toghether.
+ * The RIB is build with one aspect in mind. Speed -- actually update speed.
+ * Therefor one thing needs to be absolutely avoided, long table walks.
+ * This is achieved by heavily linking the different parts toghether.
  */
 
 struct rib_stats {
