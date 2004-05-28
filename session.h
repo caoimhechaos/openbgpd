53bb4805c1dc816d5a61a640455b6a7c63cd7ae4
henning <henning>
2004-05-28 18:39:09 +0000

    detect absence of PF_KEY interface and/or the TCP_MD5SIG setsockopts at runtime and disable said subsystems if so. helps the guys porting bgpd to $otherBSD, and is actually the right thing to do. claudio ok

--- session.h
+++ session.h
@@ -123,6 +123,11 @@ struct capa_mp {
 	u_int8_t		safi;
 };
 
+struct bgpd_sysdep {
+	u_int8_t		no_pfkey;
+	u_int8_t		no_md5sig;
+};
+
 struct ctl_conn {
 	TAILQ_ENTRY(ctl_conn)	entries;
 	struct imsgbuf		ibuf;
@@ -216,9 +221,9 @@ void	control_accept(int);
 void	control_close(int);
 
 /* pfkey.c */
-int	pfkey_establish(struct peer *p);
-int	pfkey_remove(struct peer *p);
-int	pfkey_init(void);
+int	pfkey_establish(struct peer *);
+int	pfkey_remove(struct peer *);
+int	pfkey_init(struct bgpd_sysdep *);
 
 /* printconf.c */
 void	print_config(struct bgpd_config *, struct network_head *, struct peer *,
