76acb8c595174b71682cb79f250e96463d910777
henning <henning>
2005-03-31 12:13:07 +0000

    zap some includes we don't need, Alexander von Gernler <grunk@pestilenz.org>

--- log.c
+++ log.c
@@ -16,11 +16,6 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
 #include <err.h>
 #include <errno.h>
 #include <netdb.h>
