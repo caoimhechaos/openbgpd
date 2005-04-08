87cf85ccac71cee375b6c5969f8c8e49da2bcde5
henning <henning>
2005-04-08 00:07:18 +0000

    don't whine when we can't find interface with index 0 (that is a rare corner case in that context, but still)

--- kroute.c
+++ kroute.c
@@ -778,10 +778,11 @@ kroute_validate(struct kroute *kr)
 	struct kif_node		*kif;
 
 	if ((kif = kif_find(kr->ifindex)) == NULL) {
-		log_warnx("interface with index %d not found, "
-		    "referenced from route for %s/%u",
-		    kr->ifindex, inet_ntoa(kr->prefix),
-		    kr->prefixlen);
+		if (kr->ifindex)
+			log_warnx("interface with index %d not found, "
+			    "referenced from route for %s/%u",
+			    kr->ifindex, inet_ntoa(kr->prefix),
+			    kr->prefixlen);
 		return (1);
 	}
 
