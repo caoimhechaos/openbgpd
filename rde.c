11668fa91f15295253dde3d0cd9d365dee1cfded
claudio <claudio>
2005-09-21 13:35:03 +0000

    Make sure that "updates" with only MP withdraws (MP_UNREACH_NRLI) stop processing after the MP withdraw block. OK henning@

--- rde.c
+++ rde.c
@@ -704,7 +704,8 @@ rde_update_dispatch(struct imsg *imsg)
 			log_peer_warnx(&peer->conf, "bad AFI, IPv4 disabled");
 			rde_update_err(peer, ERR_UPDATE, ERR_UPD_OPTATTR,
 			    NULL, 0);
-			path_put(asp);
+			if (attrpath_len != 0)
+				path_put(asp);
 			return (-1);
 		}
 
@@ -777,6 +778,11 @@ rde_update_dispatch(struct imsg *imsg)
 		default:
 			/* silently ignore unsupported multiprotocol AF */
 			break;
+		}
+
+		if ((asp->flags & ~F_ATTR_MP_UNREACH) == 0) {
+			path_put(asp);
+			return (0);
 		}
 	}
 
