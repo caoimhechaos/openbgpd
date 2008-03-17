ec7f1d362d9062bd368132385464f5e658150fc1
henning <henning>
2008-03-17 20:40:04 +0000

    two small bugs in printing funcs: log_as < vs <= confusion, AS 65535 printed like 4 byte AS 0.65535 aspath_strlen: omitted stripping high 16 bits of 32bit AS after dealing with the upper half From: Matthew Dempsky <matthew@dempsky.org>

--- util.c
+++ util.c
@@ -79,7 +79,7 @@ log_as(u_int32_t as)
 {
 	static char	buf[12];	/* "65000.65000\0" */
 
-	if (as < USHRT_MAX) {
+	if (as <= USHRT_MAX) {
 		if (snprintf(buf, sizeof(buf), "%u", as) == -1)
 			return ("?");
 	} else {
@@ -212,6 +212,7 @@ aspath_strlen(void *data, u_int16_t len)
 				else
 					total_size += 1;
 				total_size += 1; /* dot between hi & lo */
+				as &= 0xffff;
 			}
 			if (as >= 10000)
 				total_size += 5;
