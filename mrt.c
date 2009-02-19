a463f436b12ed8882d00918302eacbbfb2e6dcfa
claudio <claudio>
2009-02-19 21:34:40 +0000

    Use a bob sized header for IPv6 not the default pedro sized one. Seems like nobody ever tried to dump IPv6 tables. Found while reworking this code.

--- mrt.c
+++ mrt.c
@@ -358,7 +358,7 @@ mrt_dump_entry_mp(struct mrt *mrt, struct prefix *p, u_int16_t snum,
 		len += MRT_BGP4MP_IPv4_ENTRY_SIZE + p_len + attr_len;
 		break;
 	case AF_INET6:
-		len += MRT_BGP4MP_IPv4_ENTRY_SIZE + p_len + attr_len;
+		len += MRT_BGP4MP_IPv6_ENTRY_SIZE + p_len + attr_len;
 		break;
 	default:
 		return (-1);
