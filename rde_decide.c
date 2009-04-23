eeaedd504727776f72cc076cf345d8db2765c42a
claudio <claudio>
2009-04-23 19:23:27 +0000

    Rework the way we handle announced networks. Instead of two freak rde_peers use one that is less freaky. Merge bgpctl and config networks into one tree. First step of a larger change in the RDE and this goes now in to allow to move forward.

--- rde_decide.c
+++ rde_decide.c
@@ -201,9 +201,16 @@ prefix_cmp(struct prefix *p1, struct prefix *p2)
 		    &p2->aspath->peer->remote_addr,
 		    sizeof(p1->aspath->peer->remote_addr)));
 
+	/* 12. for announced prefixes prefer dynamic routes */
+	if ((p1->flags & F_ANN_DYNAMIC) != (p2->flags & F_ANN_DYNAMIC)) {
+		if (p1->flags & F_ANN_DYNAMIC)
+			return (1);
+		else
+			return (-1);
+	}
+
 	fatalx("Uh, oh a politician in the decision process");
 	/* NOTREACHED */
-	return (0);
 }
 
 /*
